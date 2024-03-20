8f64058512daa4cb52ee2b08d69e4b870e7476e3
Yang <zhy0216@gmail.com>
2024-03-20 23:16:05 +0800

    lint

--- examples/echo/index.ts
+++ examples/echo/index.ts
@@ -25,11 +25,12 @@ class BunApp {
               },
             },
           );
-          // TODO: Match type error here
-          return fn({ ...req, params: {}, query }).then<Response>((result) => (result instanceof Response ? result : new Response(result as any)));
+          // TODO: Match type error here. 1. match return fn did not match get 2. as any
+          // @ts-ignore
+          return fn({ ...req, params: {}, query }).then<Response>((result) => (result instanceof Response ? result : new Response(result)));
         }
 
-        return new Response(`404!`);
+        return new Response("404!");
       },
     });
   }
